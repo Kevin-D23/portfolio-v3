437283f8fd19a2c0ae676752011317d5e164769a
Kevin-D23 <116064184+Kevin-D23@users.noreply.github.com>
2025-03-01 18:00:42 -0800

    fix: inserted analytics component

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,17 @@ import "./App.css";
 import HomePage from "./components/home_page";
 import Admin from "./components/admin";
 import { Route, Routes, useLocation } from "react-router-dom";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   const location = useLocation();
   return (
     <div className="App">
       <Routes location={location} key={location.pathname}>
-        <Route element={<HomePage />} path="*"/>
-        <Route element={<Admin />} path="/admin"/>
+        <Route element={<HomePage />} path="*" />
+        <Route element={<Admin />} path="/admin" />
       </Routes>
+      <Analytics />
     </div>
   );
 }
